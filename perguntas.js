4aa37bdc8ba8ea0b25b2c7f80dc764c2df027f6b
gabis2510 <143627210+gabis2510@users.noreply.github.com>
2024-09-18 12:20:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz suco de laranja em Inglês?',
+    'Suco de laranja é Orange Juice'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz academia em Inglês?',
+    'Academia em inglês é Gym'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz sempre em Inglês?',
+    'Sempre em inglês é Always'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz nunca em Inglês?',
+    'Nunca em inglês é Never'
+)
